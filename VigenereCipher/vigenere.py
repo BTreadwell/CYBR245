398d04511de0f2d0d25603a82068318dd7439eba
Brennon Treadwell <bjt1798@uncw.edu>
2024-01-22 15:32:53 -0500

    Vigenere - no decrypt

---
+++ VigenereCipher/vigenere.py
@@ -0,0 +1,26 @@
+UPPER_OFFSET = 65
+LOWER_OFFSET = 97
+
+
+def encrypt(message: str, keyword: str) -> str:
+    """
+    Encrypts a message using the Vigenere cipher with the provided keyword
+    :param message: str
+    :param keyword: str, upper-case English characters
+    :return: str
+    """
+
+    ciphertext = ""
+    for idx, char in enumerate(message):
+        char_ascii = ord(char)
+        shift = ord(keyword[idx % len(keyword)]) - UPPER_OFFSET
+
+        if char.isupper():
+            ciphertext += chr(((char_ascii - UPPER_OFFSET) + shift) % 26 + UPPER_OFFSET)
+        elif char.islower():
+            ciphertext += chr(((char_ascii - LOWER_OFFSET) + shift) % 26 + LOWER_OFFSET)
+        else:
+            ciphertext += char
+
+    return ciphertext
+
---
+++ VigenereCipher/vigenere_driver.py
@@ -0,0 +1,20 @@
+import vigenere
+
+
+def main():
+    is_encrypt = input("Would you like to encrypt (e) or decrypt (d): ").strip().upper() == 'E'
+
+    message = input("Enter the message: ")
+    key = input("Enter the encryption key: ").upper()
+
+    while not key.isalpha():
+        print("Key must contain only alphabetic characters A-Z")
+        key = input("Enter the encryption key: ").upper()
+
+    new_message = Vigenere.apply_vigenere(is_encrypt, message, key)
+
+    print("The result of applying the Vigenere Cipher with the provided parameters is:\n" + new_message)
+
+
+if __name__ == "__main__":
+    main()
